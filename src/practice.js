7a3073e5ae078550128ca5a3c9e9634ddb999a9e
Ramarduk <ramarduk@gmail.com>
2019-08-28 15:48:20 -0600

    query updates

---
+++ src/practice.js
@@ -0,0 +1,20 @@
+'use strict';
+
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+});
+
+console.log('connection successful');
+
+const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({ name: 'Point of view gun' })
+  .first()
+  .toQuery();
+
+console.log(qry);
